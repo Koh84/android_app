de8f08d36b465643e4bb950c1af8d7be57bc95c2
kelvinkoh <koh84@hotmail.com>
2017-12-04 23:43:35 +0000

    v7, using ILedService to access LED

--- APP_0001_LEDDemo/app/src/main/java/com/example/kelvin/app_0001_leddemo/MainActivity.java
+++ APP_0001_LEDDemo/app/src/main/java/com/example/kelvin/app_0001_leddemo/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.kelvin.app_0001_leddemo;
 
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
-import com.example.kelvin.hardlibrary.*;
+import android.os.ILedService;
+import android.os.ServiceManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private CheckBox checkBoxLed2 = null;
     private CheckBox checkBoxLed3 = null;
     private CheckBox checkBoxLed4 = null;
+    private ILedService iLedService = null;
 
     class MyButtonListener implements View.OnClickListener {
         @Override
@@ -28,8 +31,12 @@ public class MainActivity extends AppCompatActivity {
                 checkBoxLed3.setChecked(true);
                 checkBoxLed4.setChecked(true);
 
-                for(int i = 0; i<4; i++)
-                    HardControl.ledCtrl(i,1);
+                try {
+                    for(int i = 0; i<4; i++)
+                        iLedService.ledCtrl(i,1);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
             }
             else{
                 button.setText("ALL ON");
@@ -38,8 +45,12 @@ public class MainActivity extends AppCompatActivity {
                 checkBoxLed3.setChecked(false);
                 checkBoxLed4.setChecked(false);
 
-                for(int i = 0; i<4; i++)
-                    HardControl.ledCtrl(i,0);
+                try {
+                    for(int i = 0; i<4; i++)
+                        iLedService.ledCtrl(i,0);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
@@ -48,56 +59,60 @@ public class MainActivity extends AppCompatActivity {
         // Is the view now checked?
         boolean checked = ((CheckBox) view).isChecked();
 
-        // Check which checkbox was clicked
-        switch(view.getId()) {
-            case R.id.LED1:
-                if (checked) {
-                    // Put some meat on the sandwich
-                    Toast.makeText(getApplicationContext(), "LED1 on", Toast.LENGTH_SHORT).show();
-                    HardControl.ledCtrl(0,1);
-                }
-                else {
-                    // Remove the meat
-                    Toast.makeText(getApplicationContext(), "LED1 off", Toast.LENGTH_SHORT).show();
-                    HardControl.ledCtrl(0,0);
-                }
-                break;
-            case R.id.LED2:
-                if (checked) {
-                    // Put some meat on the sandwich
-                    Toast.makeText(getApplicationContext(), "LED2 on", Toast.LENGTH_SHORT).show();
-                    HardControl.ledCtrl(1,1);
-                }
-                else {
-                    // Remove the meat
-                    Toast.makeText(getApplicationContext(), "LED2 off", Toast.LENGTH_SHORT).show();
-                    HardControl.ledCtrl(1,0);
-                }
-                break;
-            case R.id.LED3:
-                if (checked) {
-                    // Put some meat on the sandwich
-                    Toast.makeText(getApplicationContext(), "LED3 on", Toast.LENGTH_SHORT).show();
-                    HardControl.ledCtrl(2,1);
-                }
-                else {
-                    // Remove the meat
-                    Toast.makeText(getApplicationContext(), "LED3 off", Toast.LENGTH_SHORT).show();
-                    HardControl.ledCtrl(2,0);
-                }
-                break;
-            case R.id.LED4:
-                if (checked) {
-                    // Put some meat on the sandwich
-                    Toast.makeText(getApplicationContext(), "LED4 on", Toast.LENGTH_SHORT).show();
-                    HardControl.ledCtrl(3,1);
-                }
-                else {
-                    // Remove the meat
-                    Toast.makeText(getApplicationContext(), "LED4 off", Toast.LENGTH_SHORT).show();
-                    HardControl.ledCtrl(3,0);
-                }
-                break;
+        try {
+            // Check which checkbox was clicked
+            switch(view.getId()) {
+                case R.id.LED1:
+                    if (checked) {
+                        // Put some meat on the sandwich
+                        Toast.makeText(getApplicationContext(), "LED1 on", Toast.LENGTH_SHORT).show();
+                        iLedService.ledCtrl(0,1);
+                    }
+                    else {
+                        // Remove the meat
+                        Toast.makeText(getApplicationContext(), "LED1 off", Toast.LENGTH_SHORT).show();
+                        iLedService.ledCtrl(0,0);
+                    }
+                    break;
+                case R.id.LED2:
+                    if (checked) {
+                        // Put some meat on the sandwich
+                        Toast.makeText(getApplicationContext(), "LED2 on", Toast.LENGTH_SHORT).show();
+                        iLedService.ledCtrl(1,1);
+                    }
+                    else {
+                        // Remove the meat
+                        Toast.makeText(getApplicationContext(), "LED2 off", Toast.LENGTH_SHORT).show();
+                        iLedService.ledCtrl(1,0);
+                    }
+                    break;
+                case R.id.LED3:
+                    if (checked) {
+                        // Put some meat on the sandwich
+                        Toast.makeText(getApplicationContext(), "LED3 on", Toast.LENGTH_SHORT).show();
+                        iLedService.ledCtrl(2,1);
+                    }
+                    else {
+                        // Remove the meat
+                        Toast.makeText(getApplicationContext(), "LED3 off", Toast.LENGTH_SHORT).show();
+                        iLedService.ledCtrl(2,0);
+                    }
+                    break;
+                case R.id.LED4:
+                    if (checked) {
+                        // Put some meat on the sandwich
+                        Toast.makeText(getApplicationContext(), "LED4 on", Toast.LENGTH_SHORT).show();
+                        iLedService.ledCtrl(3,1);
+                    }
+                    else {
+                        // Remove the meat
+                        Toast.makeText(getApplicationContext(), "LED4 off", Toast.LENGTH_SHORT).show();
+                        iLedService.ledCtrl(3,0);
+                    }
+                    break;
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
         }
     }
 
@@ -106,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        HardControl.ledOpen();
+        iLedService = ILedService.Stub.asInterface(ServiceManager.getService("led"));
 
         button = (Button) findViewById(R.id.BUTTON);
         button.setOnClickListener(new MyButtonListener());
